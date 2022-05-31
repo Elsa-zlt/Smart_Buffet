0ef5aec1a25e04cd1111ccdc78b2093e9fb58d2a
“Elsa-zlt” <2903250242@qq.com>
2022-05-31 22:39:51 +0800

    Initial commit

---
+++ src/main/java/com/elsa/smart_buffet/SmartBuffetApplication.java
@@ -0,0 +1,13 @@
+package com.elsa.smart_buffet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBuffetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBuffetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elsa/smart_buffet/SmartBuffetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsa.smart_buffet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBuffetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
